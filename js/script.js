c14017920f5854c4808a483cd3cc00fe4d6cbb12
Jimmy Indrianto <jimmy.indrianto@example.com>
2024-08-01 23:36:22 +0700

    create index

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 /*  Ini Javascript */
+
+function formValidation() {    
+    let name = document.getElementById('input-name').value;
+    console.log(name);
+
+    // ini adalah  pengecekan dimana name tidak boleh kosong ''
+    if (name == '') {
+        // Code ini iakan di eksekusi ketika name = kosong ''
+        alert("Maaf inputan anda kosong");
+    } else {
+        // Code ini iakan di eksekusi ketika name tidak kosong ''
+        alert('Sukses menginput');
+    }
+
+}
+
+showSlide();
+function showSlide() {
+    let listImage = document.getElementsByClassName('main-content-banner');
+    console.log(listImage);
+}
